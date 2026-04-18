b8625e211187d8ff4a7dca5ba343d236fb55790d
Salsa Bila <salsabilaaisyazulhijjah@gmail.com>
2026-04-18 12:57:10 +0800

    perubahan

--- 25-function-argument.php
+++ 25-function-argument.php
@@ -10,3 +10,18 @@
 // }
 
 // checkStatusKelulusan(70, 72);
+
+//function LuasPersegiPanjang($panjang, $lebar){
+//    $Luas = $panjang * $lebar;
+//    return $Luas;
+//}
+
+
+function LuasPersegiPanjang($panjang, $lebar){
+    $Luas = $panjang * $lebar;
+    return $Luas;
+}
+$panjang = 10;
+$lebar = 5;
+$luas = LuasPersegiPanjang($panjang, $lebar);
+echo "Luas Persegi Panjang: " . $luas;
